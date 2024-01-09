126a2e97c64ca886ff9250f793133c0b3609188c
Mr. Orleond <mr.orleond@gmail.com>
2024-01-09 15:51:33 +0300

    Вычисление количества дней до дня рождения через функцию c учетом, что день рождения мог уже пройти. Дни рождения находятся в списке кортежей

--- 28. bdays.py
+++ 28. bdays.py
@@ -1,15 +1,24 @@
 import datetime as dtime
 
+FORMAT = '%d.%m.%Y'
 lera = dtime.date(2015, 5, 16)
 maxim = dtime.date(2011, 12, 16)
 
-def get_days_to_birthday(date_birthday):
+birthdays = [
+    ('Лера', '16.05.2015'),
+    ('Максим', '16.12.2011'),
+    ('Сергей', '21.10.1991')
+]
+
+
+def get_days_to_birthday(name, date_birthday):
     today = dtime.date.today()
     today_year = today.year
-    date_birthday = date_birthday.replace(year = today_year)
+    date_birthday = date_birthday.replace(year=today_year)
     if date_birthday < today:
-        date_birthday = date_birthday.replace(year = today_year + 1)
-    return date_birthday - today
+        date_birthday = date_birthday.replace(year=today_year + 1)
+    return f'{name}, до твоего дня рождения осталось дней: {date_birthday - today}'
 
-print(get_days_to_birthday(lera))
-print(get_days_to_birthday(maxim))
+
+for values in birthdays:
+    print(get_days_to_birthday(values[0], dtime.datetime.strptime(values[1], FORMAT).date()))
