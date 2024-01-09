a28538332fb3d75b12d8705aebc3cf15cb310674
Mr. Orleond <mr.orleond@gmail.com>
2024-01-09 15:28:48 +0300

    Вычисление количества дней до дня рождения через функцию

--- 28. bdays.py
+++ 28. bdays.py
@@ -3,13 +3,11 @@ import datetime as dtime
 lera = dtime.date(2015, 5, 16)
 maxim = dtime.date(2011, 12, 16)
 
-today = dtime.date.today()
-today_year = today.year
+def get_days_to_birthday(date_birthday):
+    today = dtime.date.today()
+    today_year = today.year
+    date_birthday = date_birthday.replace(year = today_year)
+    return date_birthday - today
 
-lera = lera.replace(year = today_year)
-maxim = maxim.replace(year = today_year)
-lera_days_left = lera - today
-maxim_days_left = maxim - today
-
-print(lera_days_left)
-print(maxim_days_left)
+print(get_days_to_birthday(lera))
+print(get_days_to_birthday(maxim))
