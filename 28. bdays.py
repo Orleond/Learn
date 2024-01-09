4e464f5eb2f4e012a8a9e3c600b697690765a24e
Mr. Orleond <mr.orleond@gmail.com>
2024-01-09 15:23:55 +0300

    Вычисление количества дней до дня рождения

--- 28. bdays.py
+++ 28. bdays.py
@@ -1,6 +1,15 @@
 import datetime as dtime
+
 lera = dtime.date(2015, 5, 16)
-max = dtime.date(2011, 12, 16)
+maxim = dtime.date(2011, 12, 16)
 
 today = dtime.date.today()
-print(today)
+today_year = today.year
+
+lera = lera.replace(year = today_year)
+maxim = maxim.replace(year = today_year)
+lera_days_left = lera - today
+maxim_days_left = maxim - today
+
+print(lera_days_left)
+print(maxim_days_left)
